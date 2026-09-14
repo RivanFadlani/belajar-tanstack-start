75ef49ab446779cbee6d5834deda6db277513723
RivanFadlani <rivanfadlani7@gmail.com>
2026-09-14 17:59:44 +0700

    feat(ui): add form with controlled inputs to create route

--- src/routes/create.tsx
+++ src/routes/create.tsx
@@ -1,14 +1,30 @@
 import Main from '#/components/layout/main'
 import { Navbar } from '#/components/navbar'
+import { Button } from '#/components/ui/button'
+import { Field } from '#/components/ui/field'
+import { Input } from '#/components/ui/input'
 import { Separator } from '#/components/ui/separator'
+import { Textarea } from '#/components/ui/textarea'
 import { createFileRoute } from '@tanstack/react-router'
 import { MoveLeft } from 'lucide-react'
+import { useState } from 'react'
 
 export const Route = createFileRoute('/create')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  // Controlled Approach (state, onChange)
+  const [title, setTitle] = useState('')
+  const [note, setNote] = useState('')
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    console.log('Ini Title:', title)
+    console.log('Ini Note:', note)
+  }
+
   return (
     <>
       <Navbar.Root>
@@ -20,6 +36,38 @@ function RouteComponent() {
 
       <Main>
         <Separator className="mb-4" />
+
+        <form onSubmit={handleSubmit}>
+          <Field.Set>
+            <Field.Group>
+              <Field.Root>
+                <Field.Label htmlFor="title">Title</Field.Label>
+                <Input
+                  id="title"
+                  type="text"
+                  placeholder="Input a Title Here!"
+                  className="bg-zinc-50 placeholder:text-zinc-400"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </Field.Root>
+              <Field.Root>
+                <Field.Label htmlFor="note">Note</Field.Label>
+                <Textarea
+                  id="note"
+                  placeholder="Input Your Note Here!"
+                  rows={5}
+                  className="bg-zinc-50 placeholder:text-zinc-400"
+                  value={note}
+                  onChange={(e) => setNote(e.target.value)}
+                />
+              </Field.Root>
+            </Field.Group>
+          </Field.Set>
+          <Button type="submit" variant="outline" className="mt-6 w-full">
+            Create
+          </Button>
+        </form>
       </Main>
     </>
   )
