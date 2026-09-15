675e83802eb94c664edfd01fad3a3c664d338594
RivanFadlani <rivanfadlani7@gmail.com>
2026-09-15 08:35:56 +0700

    refactor(create): use shared NoteForm component

---
+++ src/components/note-form.tsx
@@ -0,0 +1,59 @@
+import { Button } from '#/components/ui/button'
+import { Field } from '#/components/ui/field'
+import { Input } from '#/components/ui/input'
+import { Textarea } from '#/components/ui/textarea'
+import type { FieldErrors } from '#/schemas/note'
+import type { Form } from '@base-ui/react/form'
+import type React from 'react'
+
+const NoteForm = ({
+  onSubmit,
+  errors,
+  datas,
+}: React.ComponentProps<typeof Form> & {
+  errors: FieldErrors
+  datas: { title?: string; note?: string }
+}) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <Field.Set>
+        <Field.Group>
+          {/* errors.title awalnya ga punya data (object kosong -> undefined (thruty)), jadi === true */}
+          {/* lalu diberi double bang (!!) untuk ubah boolean menjadi ke nilai sebaliknya, jadi === false */}
+          {/* supaya awal form di-load, status field tidak langsung data-invalid='true' */}
+          <Field.Root data-invalid={!!errors.title}>
+            <Field.Label htmlFor="title">Title</Field.Label>
+            <Input
+              id="title"
+              name="title"
+              type="text"
+              placeholder="Input a Title Here!"
+              className="bg-zinc-50 placeholder:text-zinc-400"
+              aria-invalid={!!errors.title}
+              defaultValue={datas?.title}
+            />
+            <Field.Error>{errors.title}</Field.Error>
+          </Field.Root>
+          <Field.Root data-invalid={!!errors.note}>
+            <Field.Label htmlFor="note">Note</Field.Label>
+            <Textarea
+              id="note"
+              name="note"
+              placeholder="Input Your Note Here!"
+              rows={5}
+              className="bg-zinc-50 placeholder:text-zinc-400"
+              aria-invalid={!!errors.note}
+              defaultValue={datas?.note}
+            />
+            <Field.Error>{errors.note}</Field.Error>
+          </Field.Root>
+        </Field.Group>
+      </Field.Set>
+      <Button type="submit" variant="outline" className="mt-6 w-full">
+        Create
+      </Button>
+    </form>
+  )
+}
+
+export default NoteForm
--- src/routes/create.tsx
+++ src/routes/create.tsx
@@ -1,27 +1,17 @@
 import Main from '#/components/layout/main'
 import { Navbar } from '#/components/navbar'
 import { Button } from '#/components/ui/button'
-import { Field } from '#/components/ui/field'
-import { Input } from '#/components/ui/input'
 import { Separator } from '#/components/ui/separator'
-import { Textarea } from '#/components/ui/textarea'
 import { createFileRoute, Link } from '@tanstack/react-router'
 import { MoveLeft } from 'lucide-react'
-import { useState } from 'react'
 import z from 'zod'
+import { useState } from 'react'
+import NoteForm from '#/components/note-form'
+import { noteSchema, type FieldErrors } from '#/schemas/note'
 
 export const Route = createFileRoute('/create')({
   component: RouteComponent,
 })
-
-const noteSchema = z.object({
-  title: z.string().min(1, { error: 'Title is required' }),
-  note: z.string().min(8, { error: 'Note minimal character is 8' }),
-})
-
-type Note = z.infer<typeof noteSchema>
-
-type FieldErrors = z.core.$ZodFlattenedError<Note>['fieldErrors']
 
 function RouteComponent() {
   // Uncontrolled Approach (formData, name)
@@ -64,42 +54,14 @@ function RouteComponent() {
       <Main>
         <Separator className="mb-4" />
 
-        <form onSubmit={handleSubmit}>
-          <Field.Set>
-            <Field.Group>
-              {/* errors.title awalnya ga punya data (object kosong -> undefined (thruty)), jadi === true */}
-              {/* lalu diberi double bang (!!) untuk ubah boolean menjadi ke nilai sebaliknya, jadi === false */}
-              {/* supaya awal form di-load, status field tidak langsung data-invalid='true' */}
-              <Field.Root data-invalid={!!errors.title}>
-                <Field.Label htmlFor="title">Title</Field.Label>
-                <Input
-                  id="title"
-                  name="title"
-                  type="text"
-                  placeholder="Input a Title Here!"
-                  className="bg-zinc-50 placeholder:text-zinc-400"
-                  aria-invalid={!!errors.title}
-                />
-                <Field.Error>{errors.title}</Field.Error>
-              </Field.Root>
-              <Field.Root data-invalid={!!errors.note}>
-                <Field.Label htmlFor="note">Note</Field.Label>
-                <Textarea
-                  id="note"
-                  name="note"
-                  placeholder="Input Your Note Here!"
-                  rows={5}
-                  className="bg-zinc-50 placeholder:text-zinc-400"
-                  aria-invalid={!!errors.note}
-                />
-                <Field.Error>{errors.note}</Field.Error>
-              </Field.Root>
-            </Field.Group>
-          </Field.Set>
-          <Button type="submit" variant="outline" className="mt-6 w-full">
-            Create
-          </Button>
-        </form>
+        <NoteForm
+          onSubmit={handleSubmit}
+          errors={errors}
+          datas={{
+            title: 'This is a title',
+            note: 'This is a Note. You can fill this textarea with your ideas!',
+          }}
+        />
       </Main>
     </>
   )
