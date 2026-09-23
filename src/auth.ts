ef260cafa7151aa17972ad0c3c0ed5055623d0c3
RivanFadlani <rivanfadlani7@gmail.com>
2026-09-23 12:47:44 +0700

    feat(auth): add getCurrentUser and authenticate server functions

---
+++ src/auth.ts
@@ -0,0 +1,35 @@
+import { createServerFn, createServerOnlyFn } from '@tanstack/react-start'
+import { useAppSession } from './lib/session'
+import { db } from '.'
+import bcrypt from 'bcryptjs'
+
+export const getCurrentUserFn = createServerFn({ method: 'GET' }).handler(
+  async () => {
+    const session = await useAppSession()
+    const userId = session.data.userId
+
+    if (!userId) {
+      return null
+    }
+
+    const user = await db.query.usersTable.findFirst({
+      where: { id: userId },
+    })
+
+    return user ? { id: user.id } : null
+  },
+)
+
+export const authenticateUser = createServerOnlyFn(
+  async (email: string, password: string) => {
+    const user = await db.query.usersTable.findFirst({
+      where: { email },
+    })
+
+    if (!user) return null
+
+    const isValid = await bcrypt.compare(password, user.password)
+
+    return isValid ? user : null
+  },
+)
